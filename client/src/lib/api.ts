a8286b45c5a11d085d3c384d999b3b11ddc9a67d
niharika <142niharikachauhan@gmai.com>
2026-05-06 23:21:59 +0530

    changes in api.ts

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:4000/api',
+  baseURL: https://task-manager-production-ce2a.up.railway.app/api,
   withCredentials: true,
 })
 
